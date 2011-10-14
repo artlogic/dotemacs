101a1324f87ad9a8b0abe37988d575e5004ff5c6
kruthjam <kruthjam@AIS-kruthjam.ais.ad.msu.edu>
2011-10-14 13:28:24 -0400

    Added markdown mode, show-paren-mode, and made path references relative to init.el.

--- init.el
+++ init.el
@@ -1,5 +1,8 @@
 ;; James Kruth's Emacs config
 
+;; figure out where we've been loaded from
+(setq dotfiles-dir (file-name-directory load-file-name))
+
 ;; Disable some unneeded visual stuff
 (setq inhibit-startup-screen t)
 (tool-bar-mode -1)
@@ -7,7 +10,10 @@
 ;; Optional mode lines
 (column-number-mode t)
 
-;; Backup file settings
+;; Global minor modes
+(show-paren-mode t)
+
+;; Backup file settings - for non vc folders
 ;;
 ;; Enable numbered version control
 ;; Store all the backups in ~/.emacs.d/backups
@@ -20,8 +26,13 @@
 ;; tramp-backup-directory-alist
 (setq 
  version-control t
- backup-directory-alist '(("." . "~/.emacs.d/backups"))
+ backup-directory-alist `(("." . ,(concat dotfiles-dir "backups")))
  kept-new-versions 8
  delete-old-versions t
  backup-by-copying-when-mismatch t)
- 
+
+;; markdown mode 
+(add-to-list 'load-path (concat dotfiles-dir "markdown-mode"))
+(autoload 'markdown-mode "markdown-mode.el"
+  "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
